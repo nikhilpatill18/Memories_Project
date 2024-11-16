96fbfa9e9b69298e66e047b853a6d54a10bd9390
nikhilpatill18 <nikhilpatil6060@gmail.com>
2024-11-16 12:27:03 +0530

    added talwind css

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,13 +1,16 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import Form from './components/form/Form'
+import './index.css'; // This should import your Tailwind styles
+
 
 
 function App() {
 
   return (
     <>
-      APp
+      <Form />
     </>
   )
 }
---
+++ Client/src/components/form/Form.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Form = () => {
+    return (
+        <div className='text-yellow-600 bg-rose-500 p-16'>
+            this is form
+        </div>
+    )
+}
+
+export default Form
